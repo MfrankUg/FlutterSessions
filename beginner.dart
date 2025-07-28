b029b6c03b2e9ed7afd556f893931b1c3e879b49
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-07-28 21:51:26 +0300

    changes

--- beginner.dart
+++ beginner.dart
@@ -1,3 +1,8 @@
 void main() {
+  // This is a simple Dart program that prints "Hello_world" to the console.
+
+  /*   This is a multi-line comment.
+   It can span multiple lines and is useful for providing detailed explanations.
+  */
   print("Hello_world");
 }
