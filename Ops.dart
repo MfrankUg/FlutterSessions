b648307b9fe07a13d0dd55f33ba4d6cef79ee779
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-07-31 13:32:20 +0300

    changes

---
+++ Ops.dart
@@ -0,0 +1,13 @@
+// Data comparison Operations
+void main() {
+  int teamScoreA = 250;
+  int teamScoreB = 300;
+  print(teamScoreB/ teamScoreA); // Division operation
+  print(teamScoreB % teamScoreA); // Modulus operation
+  print(teamScoreA == teamScoreB); // Equality operation
+  print(teamScoreA != teamScoreB); // Inequality operation
+  print(teamScoreA > teamScoreB); // Greater than operation
+  print(teamScoreA < teamScoreB); // Less than operation
+  print(teamScoreA >= teamScoreB); // Greater than or equal to operation
+  print(teamScoreA <= teamScoreB); // Less than or equal to operation
+}
