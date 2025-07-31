8797b363c8084c3ad9f15ab1142b25a47f94122d
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-07-31 13:39:47 +0300

    changes

--- Ops.dart
+++ Ops.dart
@@ -2,7 +2,7 @@
 void main() {
   int teamScoreA = 250;
   int teamScoreB = 300;
-  print(teamScoreB/ teamScoreA); // Division operation
+  print(teamScoreB / teamScoreA); // Division operation
   print(teamScoreB % teamScoreA); // Modulus operation
   print(teamScoreA == teamScoreB); // Equality operation
   print(teamScoreA != teamScoreB); // Inequality operation
@@ -10,4 +10,7 @@ void main() {
   print(teamScoreA < teamScoreB); // Less than operation
   print(teamScoreA >= teamScoreB); // Greater than or equal to operation
   print(teamScoreA <= teamScoreB); // Less than or equal to operation.
+
+  // Use of boleans
+  int currentFlows = 20;
 }
