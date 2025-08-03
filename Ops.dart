e7ba9dcbce3f60070ea055a2bf077e24bcab3705
FRANK MUHINDO <100847088+MfrankUg@users.noreply.github.com>
2025-08-03 20:40:20 +0300

    changes

--- Ops.dart
+++ Ops.dart
@@ -18,4 +18,14 @@ void main() {
   print(isCount); // Output: true
   bool isCount2 =  currentFlows < permittedFlows; // Boolean operation
   print(isCount2); // Output: false
+
+  // Venturing into == and != operators
+  int a = 5;
+  int b = 10;
+  bool isEqual = a == b; // Check if a is equal to b
+  bool isNotEqual = a != b; // Check if a is not equal to b
+  print("a == b: $isEqual"); // Output: a == b: false
+  print("a != b: $isNotEqual"); // Output: a != b: true
+  // The == operator checks for equality, while the != operator checks for inequality.
+  // These operators are often used in conditional statements to compare values and make decisions based on the results.
 }
