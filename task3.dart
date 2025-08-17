4ae809923fb39ed0373ecd6798146a95822313c3
MfrankUg <muhindofrank2001@gmail.com>
2025-08-17 23:14:22 +0300

    changes

---
+++ task3.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Temperature of the planet in Celsius
+  double planetTemperature = 450.37;
+
+  // TODO: Convert the temperature from a double to a String
+  String temperatureText = planetTemperature.toString();
+
+  // Logging the temperature as text
+  print("Data log: the planet's temperature is: " + temperatureText + "°C");
+}
