e33c9a86138cc3b040380405b54b68a4b83fdb71
MfrankUg <muhindofrank2001@gmail.com>
2025-08-19 14:54:41 +0300

    practice on if conditions with logical operations

--- conditionals.dart
+++ conditionals.dart
@@ -21,4 +21,24 @@ void main() {
   } else {
     print("The weather is unpredictable.");
   }
+
+  // if conditionals with logical operators
+  int temperature = 23; // Temperature in degrees Celsius
+  int humidity = 40; // Humidity in percentage
+  int dustLevel = 15; // Dust level in milligrams per cubic meter
+
+  if (temperature > 23 || humidity > 40 || dustLevel > 15) {
+    print(
+      "Warning!"
+      "The temperature: $temperature , humidity: $humidity , dustlevel: $dustLevel are too high,"
+      "Please consider doing the neccessary measures to keep the coffee seedlings safe.",
+    );
+  } else if (temperature < 23 && humidity < 40 && dustLevel < 15) {
+    print(
+      "Critical"
+      "please ensure cooling, dusting and cleaning the room to keep the coffee clean and safe.",
+    );
+  } else {
+    print("The warehouse is under normal conditions");
+  }
 }
