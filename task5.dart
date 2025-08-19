af7282d2df4ec7a879c3d4789c4fee9f16a11b76
MfrankUg <muhindofrank2001@gmail.com>
2025-08-19 12:54:14 +0300

    task5 loaded

---
+++ task5.dart
@@ -0,0 +1,12 @@
+void main() {
+  int starCount = 7;
+
+  // TODO: Convert the starCount integer to a double
+  double starCountDouble = starCount.toDouble();
+
+  // TODO: Now, convert the new double to a String
+  String starCountStr = starCountDouble.toString(); 
+
+  // Print out the String value after conversion.
+  print("In the cosmos, the star count is: " + starCountStr);
+}
